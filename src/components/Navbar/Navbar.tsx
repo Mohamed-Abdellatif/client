d042afc523d86de4710f125df0ccaf80a87f57e0
Mohamed-Abdellatif <mabdellatif411@gmail.com>
2025-07-21 18:01:07 +0300

    Refactor Navbar and extracting Searchbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,13 @@
 import React from "react";
-import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
-
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
 import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
-import { Badge } from "@mui/material";
-
 import MailIcon from "@mui/icons-material/Mail";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import { useMediaQuery, useTheme } from "@mui/material";
+import { useMediaQuery, useTheme, Badge } from "@mui/material";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
@@ -21,47 +15,9 @@ import { useDispatch } from "react-redux";
 import type { AppDispatch } from "../../store/store";
 import { logout } from "../../features/auth/AuthSlice/authSlice";
 import { useTranslation } from "react-i18next";
+import SearchBar from "../SearchBar/SearchBar";
+
 const drawerWidth = 200;
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  width: "100%",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
 
 interface NavbarProps {
   handleDrawerToggle: () => void;
@@ -71,7 +27,7 @@ const Navbar: React.FC<NavbarProps> = ({ handleDrawerToggle }) => {
   const dispatch = useDispatch<AppDispatch>();
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const { t} = useTranslation();
+  const { t } = useTranslation();
 
   // State for mobile dropdown menu
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -126,30 +82,21 @@ const Navbar: React.FC<NavbarProps> = ({ handleDrawerToggle }) => {
         {/* Only show these on desktop */}
         {!isMobile && (
           <>
-            <Search
+            <SearchBar
+              placeholder={t("search_placeholder")}
               sx={{
                 color: "#757c79",
                 boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
                 borderRadius: 8,
                 mr: 2,
               }}
-            >
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                sx={{ color: "black", borderColor: "black" }}
-                placeholder={t("search_placeholder")}
-                inputProps={{ "aria-label": "search" }}
-              />
-            </Search>
-
+            />
             <IconButton size="large" color="inherit" sx={{ color: "#42899e" }}>
               <Badge badgeContent={10} color="error">
                 <NotificationsIcon />
               </Badge>
             </IconButton>
-            <IconButton size="large" color="inherit" sx={{ color: "#42899e" }} >
+            <IconButton size="large" color="inherit" sx={{ color: "#42899e" }}>
               <Badge badgeContent={4} color="error">
                 <MailIcon />
               </Badge>
@@ -184,23 +131,15 @@ const Navbar: React.FC<NavbarProps> = ({ handleDrawerToggle }) => {
               transformOrigin={{ vertical: "top", horizontal: "right" }}
             >
               <MenuItem>
-                <Search
+                <SearchBar
+                  placeholder={t("search_placeholder")}
                   sx={{
                     color: "#757c79",
                     boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
                     borderRadius: 8,
                     my: 1,
                   }}
-                >
-                  <SearchIconWrapper>
-                    <SearchIcon />
-                  </SearchIconWrapper>
-                  <StyledInputBase
-                    sx={{ color: "black", borderColor: "black" }}
-                    placeholder={t("search_placeholder")}
-                    inputProps={{ "aria-label": "search" }}
-                  />
-                </Search>
+                />
               </MenuItem>
               <MenuItem onClick={handleMenuClose}>
                 <Badge badgeContent={10} color="error" sx={{ mr: 1 }}>
@@ -215,7 +154,8 @@ const Navbar: React.FC<NavbarProps> = ({ handleDrawerToggle }) => {
                 {t("mail")}
               </MenuItem>
               <MenuItem onClick={handleLogout}>
-                <AccountCircle sx={{ color: "#42899e", mr: 1 }} /> {t("account")}
+                <AccountCircle sx={{ color: "#42899e", mr: 1 }} />{" "}
+                {t("account")}
               </MenuItem>
             </Menu>
           </>
